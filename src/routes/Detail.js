caee406a5c73b6f1a9b76e05f54df3d887d573e7
hyeoncheolkim <hyeoncheol_kim@tmax.co.kr>
2022-12-08 16:06:37 +0900

    bugFix: useEffect dependency 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,23 +2,35 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
   const [detailMovie, setDetailMovie] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
     setDetailMovie(json.data.movie);
+    setLoading(false);
   };
+
   useEffect(() => {
     getMovie();
-  });
+  }, []);
   return (
     <>
-      <img src={detailMovie.background_image} alt="영화이미지" />
-      <p>{detailMovie.title_long}</p>
-      <p>{detailMovie.year}</p>
+      {loading ? (
+        <span>loading ...</span>
+      ) : (
+        <>
+          <img
+            src={detailMovie.background_image}
+            alt="영화이미지"
+            width={"500px"}
+          />
+          <span>{detailMovie.title_long}</span>
+          <span>{detailMovie.year}</span>
+        </>
+      )}
     </>
   );
 }
